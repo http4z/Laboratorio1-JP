bfabeb47bfda7fc6966767261e3611f2a9b4df95
JP <pazigna.gt@gmail.com>
2023-03-16 21:15:02 -0600

    Commit 1

---
+++ src/modelo/main.java
@@ -0,0 +1,23 @@
+// Alumno: Josué Ignacio Paz Moran
+// Carné: 1290-21-5213
+// Curso: Programación III
+// Fecha: 16/03/2023
+
+package modelo;
+
+/**
+ *
+ * @author paiz2
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         frm_arbol_b frm = new frm_arbol_b();
+        frm.show();
+    }
+    
+}
